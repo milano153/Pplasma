135be0ed143f91280cc33fcd2d40559b55a4b33c
milano153 <punksonsomnia@gmail.com>
2025-10-06 02:35:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1132,6 +1132,7 @@ const handleViewMyNfts = async () => {
   {viewerItems.length > 0 && (
     <>
       <div style={{ marginTop: 12, opacity: 0.75 }}>
+<s.SpacerLargeXX />
         {collectionName} {collectionSymbol ? `(${collectionSymbol})` : ""}
       </div>
       <div
